fa0129b647978ea63245b720f5be4e9cd702cc76
lorenzo medici <lorenzomedici2001@gmail.com>
2022-10-09 21:39:44 +0200

    1.0

---
+++ app/src/main/java/com/lolodmc70/HomeActivity.java
@@ -0,0 +1,273 @@
+package com.lolodmc70;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    double num1=0,num2=0;
+    boolean Addition,Decimal,Subtract,Multiplication,Remainder,Division;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_home);
+        Button AC = findViewById(R.id.AC_buttom);
+        Button plusDivMoins = findViewById(R.id.plusdivmoins_butom);
+        Button refresh = findViewById(R.id.Refresh_buttom);
+        Button mod = findViewById(R.id.mod_button);
+        Button div = findViewById(R.id.div_button);
+        Button mult = findViewById(R.id.mult_button);
+        Button moins = findViewById(R.id.moins_button);
+        Button plus = findViewById(R.id.plus_button);
+        Button equals = findViewById(R.id.equals_button);
+        Button nine = findViewById(R.id.nine_button);
+        Button eight = findViewById(R.id.eight_buttom);
+        Button seven = findViewById(R.id.seven_buttom);
+        Button six = findViewById(R.id.six_button);
+        Button five = findViewById(R.id.five_buttom);
+        Button four = findViewById(R.id.four_buttom);
+        Button three = findViewById(R.id.three_button);
+        Button two = findViewById(R.id.two_buttom);
+        Button one = findViewById(R.id.one_buttom);
+        Button zero = findViewById(R.id.zero_buttom);
+        Button point = findViewById(R.id.point_button);
+        TextView result =  findViewById(R.id.result);
+        TextView operation =  findViewById(R.id.operation);
+
+        point.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Decimal){
+
+                }else{
+            operation.setText(operation.getText()+".");
+            Decimal=true;
+                }
+            }
+        });
+
+        AC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operation.setText("");
+                num1=0;
+                num2=0;
+            }
+        });
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operation.setText("");
+                result.setText("");
+                num1=0;
+                num2=0;
+            }
+        });
+
+        equals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(Addition || Decimal || Subtract || Multiplication || Remainder || Division) {
+                    num2 = Float.parseFloat(operation.getText() + "");
+                }
+
+                if(Addition){
+                    result.setText(num1+num2+"");
+                    Addition= false;
+                }
+                if(Subtract){
+                    result.setText(num1-num2+"");
+                    Subtract= false;
+                }
+                if(Multiplication){
+                    result.setText(num1*num2+"");
+                    Multiplication= false;
+                }
+                if(Remainder){
+                    result.setText(num1%num2+"");
+                    Remainder= false;
+                }
+                if(Division){
+                    result.setText(num1/num2+"");
+                    Division= false;
+                }
+            }
+        });
+
+        plusDivMoins.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               if(operation.getText().length() !=0 ){
+
+        num1=Float.parseFloat(operation.getText()+"");
+          Addition = true;
+          Decimal = false;
+          operation.setText(null);
+               }
+            }
+        });
+
+        moins.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(operation.getText().length() !=0 ){
+
+                    num1=Float.parseFloat(operation.getText()+"");
+                    Subtract = true;
+                    Decimal = false;
+                    operation.setText(null);
+                }
+            }
+        });
+
+        mult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(operation.getText().length() !=0 ){
+
+                    num1=Float.parseFloat(operation.getText()+"");
+                    Multiplication = true;
+                    Decimal = false;
+                    operation.setText(null);
+                }
+            }
+        });
+
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(operation.getText().length() !=0 ){
+
+                    num1=Float.parseFloat(operation.getText()+"");
+                    Division = true;
+                    Decimal = false;
+                    operation.setText(null);
+                }
+            }
+        });
+
+        mod.setOnClickListener(new View.OnClickListener() {
+            @Override
+             public void onClick(View view) {
+                if(operation.getText().length() !=0 ){
+
+                    num1=Float.parseFloat(operation.getText()+"");
+                    Remainder = true;
+                    Decimal = false;
+                    operation.setText(null);
+                }
+             }
+        });
+
+        zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                displayOperation("0");
+
+            }
+        });
+
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                displayOperation("1");
+            }
+        });
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                displayOperation("2");
+            }
+        });
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                displayOperation("3");
+            }
+        });
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                displayOperation("4");
+            }
+        });
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                displayOperation("5");
+            }
+        });
+        six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                displayOperation("6");
+            }
+        });
+        seven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                displayOperation("7");
+            }
+        });
+        eight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayOperation("8");
+            }
+        });
+        nine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            displayOperation("9");
+            }
+        });
+    }
+
+
+    private void displayOperation(String val) {
+        TextView operation =  findViewById(R.id.operation);
+        operation.setText(operation.getText()+""+val);
+    }
+
+
+    /*private double add(double valOne,double valTwo) {
+        return valOne+valTwo;
+    }
+    private double sub(double valOne,double valTwo) {
+        return valOne-valTwo;
+    }
+    private double mul(double valOne,double valTwo) {
+        return valOne*valTwo;
+    }
+    private double div(double valOne,double valTwo) {
+        return valOne/valTwo;
+    }
+    private double mod(double valOne,double valTwo) {
+        return valOne%valTwo;
+    }*/
+
+
+
+}
---
+++ app/src/main/java/com/lolodmc70/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lolodmc70;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
