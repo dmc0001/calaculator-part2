7c95efc4660df43eb239fc7814d42ffac9c1d21e
lorenzo medici <lorenzomedici2001@gmail.com>
2022-10-12 21:12:01 +0200

    1.1

--- app/src/main/java/com/lolodmc70/MainActivity.java
+++ app/src/main/java/com/lolodmc70/MainActivity.java
@@ -2,13 +2,21 @@ package com.lolodmc70;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView appName;
+LottieAnimationView lottie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lottie = findViewById(R.id.animation_main);
+        lottie.playAnimation();
     }
 }
