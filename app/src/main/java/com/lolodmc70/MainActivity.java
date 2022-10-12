65d36995a2e9e21aff45c86c40ed0cc5fd0db7de
lorenzo medici <lorenzomedici2001@gmail.com>
2022-10-12 21:28:46 +0200

    1.1

--- app/src/main/java/com/lolodmc70/MainActivity.java
+++ app/src/main/java/com/lolodmc70/MainActivity.java
@@ -9,8 +9,11 @@ import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
-TextView appName;
+Timer timer;
 LottieAnimationView lottie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +21,14 @@ LottieAnimationView lottie;
         setContentView(R.layout.activity_main);
         lottie = findViewById(R.id.animation_main);
         lottie.playAnimation();
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(),HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },4000);
     }
 }
